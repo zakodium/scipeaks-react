eca021dfff413d02a0c5ae274fc9a10942d27ead
Michaël Zasso <targos@protonmail.com>
2020-12-05 12:12:29 +0100

    chore: add error boundary

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import { AppProps } from 'next/app';
 import React from 'react';
 
+import { PageErrorBoundary } from '../components/tailwind-ui';
+
 import '../../styles/index.css';
 
 function C6H6App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <PageErrorBoundary>
+      <Component {...pageProps} />
+    </PageErrorBoundary>
+  );
 }
 
 export default C6H6App;
