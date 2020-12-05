408ac5be79bb0c8eea12b8680d46ac3e7daf6fbb
Michaël Zasso <targos@protonmail.com>
2020-12-05 12:13:13 +0100

    chore: make nmr-displayer lazy for code splitting

--- src/components/nmr/EnhancedNMRDisplayer.tsx
+++ src/components/nmr/EnhancedNMRDisplayer.tsx
@@ -1,12 +1,14 @@
 // eslint-disable-next-line import/no-unresolved
-import NMRDisplayer, { NMRDisplayerProps } from 'nmr-displayer';
-import React from 'react';
+import type { NMRDisplayerProps } from 'nmr-displayer';
+import React, { lazy } from 'react';
 
 /* eslint-disable import/no-extraneous-dependencies */
 import 'cheminfo-font/dist/style.css';
 import 'react-animated-slider-2/build/horizontal.css';
 import 'prismjs/themes/prism.css';
 /* eslint-enable */
+
+const NMRDisplayer = lazy(() => import('nmr-displayer'));
 
 export default function EnhancedNMRDisplayer(props: NMRDisplayerProps) {
   return <NMRDisplayer {...props} />;
