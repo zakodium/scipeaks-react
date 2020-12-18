09499a55c0eedbaaa6d0fb7e45b1237239177e93
Michaël Zasso <targos@protonmail.com>
2020-12-18 16:09:07 +0100

    fix: update NMR displayer
    Removes external CSS and better layout in a tailwind project.

--- src/components/nmr/EnhancedNMRDisplayer.tsx
+++ src/components/nmr/EnhancedNMRDisplayer.tsx
@@ -3,11 +3,6 @@ import React, { lazy, Suspense } from 'react';
 
 import LoadingFull from '../LoadingFull';
 
-/* eslint-disable import/no-extraneous-dependencies */
-import 'cheminfo-font/dist/style.css';
-import 'prismjs/themes/prism.css';
-/* eslint-enable */
-
 const NMRDisplayer = lazy(() => import('nmr-displayer'));
 
 export default function EnhancedNMRDisplayer(props: NMRDisplayerProps) {
