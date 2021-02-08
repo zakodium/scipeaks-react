8acfbfe17e31edd40523ca4306a26ef0af8530c0
Luc Patiny <luc@patiny.com>
2021-02-08 11:00:59 +0100

    chore(pubchem): update Panels

--- src/components/chemistry/pubchem/Panels.tsx
+++ src/components/chemistry/pubchem/Panels.tsx
@@ -2,14 +2,17 @@ import { ReactElement, useState } from 'react';
 
 import { HorizontalNavigation } from '@/components/tailwind-ui';
 
+import Computed from './computed/Computed';
+import GHSFull from './ghs/full/GHSFull';
 import GHS from './ghs/summary/GHS';
 
 const tabs = [
   { value: 'ghs', label: 'GHS' },
+  { value: 'ghsFull', label: 'GHS Detailed' },
   { value: 'computed', label: 'Computed' },
 ];
 
-export default function Full(props: any) {
+export default function Panels(props: any) {
   const [selectedTab, setSelectedTab] = useState(tabs[0]);
 
   let panel: ReactElement;
@@ -17,11 +20,14 @@ export default function Full(props: any) {
     case 'ghs':
       panel = <GHS ghs={props.data.ghs} />;
       break;
+    case 'ghsFull':
+      panel = <GHSFull ghsFull={props.data.getGHS()} />;
+      break;
     case 'computed':
-      panel = <div>Computed</div>;
+      panel = <Computed computed={props.data.computed} />;
       break;
     default:
-      throw Error('Full.tsx unreachable');
+      throw Error('Panels.tsx unreachable');
   }
 
   return (
