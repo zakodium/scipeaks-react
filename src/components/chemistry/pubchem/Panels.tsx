7c8d0fbb0d905a018bf33adbfddfaa6958734ad6
Kevin Jablonka <32935233+kjappelbaum@users.noreply.github.com>
2021-05-01 14:22:05 +0200

    feat: add identifier panel in pubchem page (#20)
    * feat: show identifier summary
    
    with Luc we discussed that it might be useful to show some basic identifiers
    and a link to Pubchem to give users the possibility
    to verify that we retrieved the information for the correct compound.
    
    i noticed that the labels are not optimal atm.
    I'll fix this upstream in the Pubchem package.
    
    * feat: also show structure and description
    
    * chore: remove lint

--- src/components/chemistry/pubchem/Panels.tsx
+++ src/components/chemistry/pubchem/Panels.tsx
@@ -9,32 +9,33 @@ import Computed from './computed/Computed';
 import Experimental from './experimental/Experimental';
 import GHSFull from './ghs/full/GHSFull';
 import GHS from './ghs/summary/GHS';
+import Identifiers from './identifiers/Identifiers';
 
 const tabs: HorizontalNavigationOption[] = [
   { value: 'ghs', label: 'GHS' },
   { value: 'ghsFull', label: 'GHS Detailed' },
   { value: 'computed', label: 'Computed' },
   { value: 'experimental', label: 'Experimental' },
+  { value: 'identifiers', label: 'Identifiers' },
 ];
 
 export default function Panels(props: any) {
   const [selectedTab, setSelectedTab] = useState(tabs[0]);
-
   let panel: ReactElement;
   switch (selectedTab.value) {
     case 'ghs':
-      panel = <GHS ghs={props.data.ghs} />;
+      panel = <GHS ghs={props.data.compoundData.ghs} />;
       break;
     case 'ghsFull':
-      panel = <GHSFull ghsFull={props.data.getGHS()} />;
+      panel = <GHSFull ghsFull={props.data.compoundData.getGHS()} />;
       break;
     case 'computed':
-      panel = <Computed computed={props.data.computed} />;
+      panel = <Computed computed={props.data.compoundData.computed} />;
       break;
     case 'experimental':
       panel = (
         <Experimental
-          experimental={props.data.getExperimentalData({
+          experimental={props.data.compoundData.getExperimentalData({
             pressure: {
               targetUnits: 'mmHg',
             },
@@ -42,6 +43,14 @@ export default function Panels(props: any) {
               targetUnits: '°C',
             },
           })}
+        />
+      );
+      break;
+    case 'identifiers':
+      panel = (
+        <Identifiers
+          identifiers={props.data.compoundData.getIdentifiers()}
+          cid={props.data.compound.getCID()}
         />
       );
       break;
---
+++ src/components/chemistry/pubchem/identifiers/Identifiers.tsx
@@ -0,0 +1,86 @@
+import { MF } from 'react-mf';
+import { SmilesSvgRenderer } from 'react-ocl';
+
+import CompactTd from '@/components/common/CompactTd';
+import { Table, Th, SvgOutlineExternalLink } from '@/components/tailwind-ui';
+
+export default function Identifiers(props: { identifiers: any; cid: any }) {
+  const { identifiers, cid } = props;
+  const rows: Array<any> = [];
+  for (let key in identifiers) {
+    if (key === 'formula') {
+      rows.push({
+        key: key,
+        label: identifiers[key].label,
+        value: <MF mf={identifiers[key].value} />,
+        description: identifiers[key].description,
+      });
+    } else if (key === 'smiles') {
+      rows.push({
+        key: key,
+        label: identifiers[key].label,
+        value: identifiers[key].value,
+        description: identifiers[key].description,
+      });
+      rows.push({
+        key: 'structure',
+        label: 'structure',
+        value: <SmilesSvgRenderer smiles={identifiers[key].value} />,
+        description: '2D chemical structure derived from the SMILES.',
+      });
+    } else {
+      rows.push({
+        key: key,
+        label: identifiers[key].label,
+        value: identifiers[key].value,
+        description: identifiers[key].description,
+      });
+    }
+  }
+
+  if (cid) {
+    rows.push({
+      key: 'cid',
+      label: 'Pubchem compound ID (CID)',
+      value: (
+        <a
+          href={`https://pubchem.ncbi.nlm.nih.gov/compound/${cid}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {cid} <SvgOutlineExternalLink style={{ display: 'inline' }} />
+        </a>
+      ),
+      description: 'Unique identifer of a compound in the PubChem database.',
+    });
+  }
+  return (
+    <Table
+      Header={Header}
+      data={rows}
+      Tr={Row}
+      tableClassName="table-fixed w-1/2"
+    />
+  );
+}
+
+function Header() {
+  return (
+    <tr>
+      <Th className="w-1/4">Label</Th>
+      <Th>Value</Th>
+      <Th>Description</Th>
+    </tr>
+  );
+}
+
+function Row(props: any) {
+  const row = props.value;
+  return (
+    <tr key={row.key}>
+      <CompactTd>{row.label}</CompactTd>
+      <CompactTd>{row.value}</CompactTd>
+      <CompactTd>{row.description}</CompactTd>
+    </tr>
+  );
+}
