eda7e3788b806ce3c7749897bbcf9406d578e731
Michaël Zasso <targos@protonmail.com>
2025-07-25 14:37:37 +0200

    chore: add page list autocomplete in home

--- src/pages/dev/home/+Page.tsx
+++ src/pages/dev/home/+Page.tsx
@@ -1,10 +1,18 @@
 import { Home } from 'react-iframe-bridge';
+import { usePageContext } from 'vike-react/usePageContext';
+
+const specialPages = new Set(['/_error', '/dev/base-page', '/dev/home']);
 
 export default function HomePage() {
+  const pageContext = usePageContext();
+  const allPages = Object.keys(pageContext.globalContext.pages)
+    .map((page) => page.slice('/src/pages'.length))
+    .filter((page) => !specialPages.has(page));
   return (
     <Home
-    // rocUrl="https://demo.scipeaks.com/roc/"
-    // database="eln"
+      pages={allPages}
+      // rocUrl="https://demo.scipeaks.com/roc/"
+      // database="eln"
     />
   );
 }
